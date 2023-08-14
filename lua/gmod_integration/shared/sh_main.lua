91e914a94d829792f5dca84fe6f9978c024d40d0
Linventif <linventif@gmail.com>
2023-08-14 10:04:50 +0000

    chat cmd

--- lua/gmod_integration/shared/sh_main.lua
+++ lua/gmod_integration/shared/sh_main.lua
@@ -4,3 +4,21 @@ function gmInte.log(msg, debug)
     //format: [2021-08-01 00:00:00] [INFO] msg
 	print("[" .. os.date("%Y-%m-%d %H:%M:%S") .. "] [Garry's Mod Integration] " .. msg)
 end
+
+// Chat Command
+
+local trigger = {
+    "link",
+    "gmint",
+    "gminte"
+}
+
+hook.Add("OnPlayerChat", "gm_inte:con-chat", function(ply, strText, bTeam, bDead)
+    if (ply != LocalPlayer() ) then return end
+
+	strText = string.sub(string.lower(strText ), 2)
+    if (table.HasValue(trigger, strText)) then
+        gui.OpenURL("https://gmod-integration.com")
+		return true
+	end
+end)
