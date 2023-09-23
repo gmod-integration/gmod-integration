71121fa24c66064a9cc656f257a0a848eba2076e
Linventif <linventif@gmail.com>
2023-09-23 03:14:20 +0200

    short log name

--- lua/gmod_integration/shared/sh_main.lua
+++ lua/gmod_integration/shared/sh_main.lua
@@ -2,7 +2,7 @@
 function gmInte.log(msg, debug)
     if (debug && !gmInte.config.debug) then return end
     //format: [2021-08-01 00:00:00] [INFO] msg
-	print("[" .. os.date("%Y-%m-%d %H:%M:%S") .. "] [Garry's Mod Integration] " .. msg)
+	print("[" .. os.date("%Y-%m-%d %H:%M:%S") .. "] [Gmod Integration] " .. msg)
 end
 
 // Chat Command
