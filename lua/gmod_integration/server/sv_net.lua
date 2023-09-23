c66bfd91435cd28009e0072cb9c39a44f4c9536e
Linventif <linventif@gmail.com>
2023-09-23 02:25:56 +0200

    fix typo

--- lua/gmod_integration/server/sv_net.lua
+++ lua/gmod_integration/server/sv_net.lua
@@ -14,6 +14,7 @@ net.Receive("gmIntegration", function(len, ply)
     if !ply:IsPlayer() then return end
     local id = net.ReadUInt(8)
     local data = util.JSONToTable(net.ReadString() || "{}")
+
     // check if argument is valid
     if netFuncs[id] then
         netFuncs[id](ply, data)
