88841e7699e46b2b3f14dda63ec2751b1df2356d
Linventif <linventif@gmail.com>
2023-09-25 11:14:42 +0200

    remove chat cmd

--- lua/gmod_integration/shared/sh_main.lua
+++ lua/gmod_integration/shared/sh_main.lua
@@ -1,23 +1,9 @@
+//
 // Functions
+//
+
 function gmInte.log(msg, debug)
     if (debug && !gmInte.config.debug) then return end
     //format: [2021-08-01 00:00:00] [INFO] msg
 	print("[" .. os.date("%Y-%m-%d %H:%M:%S") .. "] [Gmod Integration] " .. msg)
 end
-
-// Chat Command
-local trigger = {
-    "link",
-    "gmint",
-    "gminte"
-}
-
-hook.Add("OnPlayerChat", "gm_inte:con-chat", function(ply, strText, bTeam, bDead)
-    if (ply != LocalPlayer() ) then return end
-
-	strText = string.sub(string.lower(strText ), 2)
-    if (table.HasValue(trigger, strText)) then
-        gui.OpenURL("https://vetif.gmod-integration.com")
-		return true
-	end
-end)
