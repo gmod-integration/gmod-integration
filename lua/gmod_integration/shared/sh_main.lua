5c66279748a0b4bf3e2fe4dd3e5e1c1b05f78f3d
Linventif <linventif@gmail.com>
2023-09-25 11:27:57 +0200

    add log error

--- lua/gmod_integration/shared/sh_main.lua
+++ lua/gmod_integration/shared/sh_main.lua
@@ -2,8 +2,15 @@
 // Functions
 //
 
+// Log
 function gmInte.log(msg, debug)
     if (debug && !gmInte.config.debug) then return end
-    //format: [2021-08-01 00:00:00] [INFO] msg
 	print("[" .. os.date("%Y-%m-%d %H:%M:%S") .. "] [Gmod Integration] " .. msg)
+end
+
+// Log Error
+function gmInte.logError(msg, debug)
+    if (debug && !gmInte.config.debug) then return end
+    print("[" .. os.date("%Y-%m-%d %H:%M:%S") .. "] [Gmod Integration] [ERROR] " .. msg)
+    print(debug.traceback())
 end
