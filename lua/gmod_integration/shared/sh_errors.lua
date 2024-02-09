d2312f55a992e3464f35d7691ba19a62e58ccfbf
Linventif <linventif@gmail.com>
2024-02-09 22:48:07 +0100

    add error reporter

---
+++ lua/gmod_integration/shared/sh_errors.lua
@@ -0,0 +1,25 @@
+//
+// Methods
+//
+
+function gmInte.sendLuaErrorReport(err, realm, stack, name, id)
+    if (name !== "gmod_integration") then return end
+    gmInte.http.post("/errors",
+        {
+            ["error"] = err,
+            ["realm"] = realm,
+            ["stack"] = stack,
+            ["name"] = name,
+            ["id"] = id,
+            ["identifier"] = SERVER && gmInte.config.id || LocalPlayer():SteamID64()
+        }
+    )
+end
+
+//
+// Hooks
+//
+
+hook.Add("OnLuaError", "gmInte:OnLuaError:SendReport", function(err, realm, stack, name, id)
+    gmInte.sendLuaErrorReport(err, realm, stack, name, id)
+end)
