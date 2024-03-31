96fa9f11903999ca986267d351c91d187c2d0c16
Linventif <linventif@gmail.com>
2024-03-31 15:51:33 +0200

    add: update version

--- lua/autorun/gmod_integration.lua
+++ lua/autorun/gmod_integration.lua
@@ -6,7 +6,7 @@ if (game.SinglePlayer()) then return print("Gmod Integration is not supported in
 
 gmInte = gmInte || {}
 
-gmInte.version = "0.3.1"
+gmInte.version = "0.3.2"
 gmInte.config = {}
 gmInte.materials = {}
 
