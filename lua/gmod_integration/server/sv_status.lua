f0f92833418530632275d5da2e67fcf2b486be60
Linventif <gregoire.becue@proton.me>
2024-07-05 22:11:12 +0000

    add: server cooldown

--- lua/gmod_integration/server/sv_status.lua
+++ lua/gmod_integration/server/sv_status.lua
@@ -10,6 +10,6 @@ function gmInte.serverShutDown()
     gmInte.http.post("/servers/:serverID/stop")
 end
 
-timer.Create("gmInte.sendStatus", 300, 0, function() gmInte.sendStatus() end)
+timer.Create("gmInte.sendStatus", 40, 0, function() timer.Simple(math.random(0, 15), function() gmInte.sendStatus() end) end)
 hook.Add("Initialize", "gmInte:Server:Initialize:SendStatus", function() timer.Simple(1, function() gmInte.serverStart() end) end)
 hook.Add("ShutDown", "gmInte:Server:ShutDown:SendStatus", function() gmInte.serverShutDown() end)
