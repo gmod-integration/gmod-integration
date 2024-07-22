b96777d01469b5c48cfa59aea285ea94a0440445
Linventif <gregoire.becue@proton.me>
2024-07-22 03:37:20 +0000

    fix: darkrp setname

--- lua/gmod_integration/server/sv_sync_names.lua
+++ lua/gmod_integration/server/sv_sync_names.lua
@@ -1,7 +1,7 @@
 function gmInte.wsSyncName(data)
     local ply = player.GetBySteamID(data.steamID64)
     if !IsValid(ply) then return end
-    ply:SetName(data.name)
+    if DarkRP then ply:setRPName(data.name) end
 end
 
 function gmInte.playerChangeName(ply, oldName, newName)
