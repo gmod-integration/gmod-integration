e3d95296b2d7d035237d3eb2cf4d7eb4bd69fbec
Linventif <gregoire.becue@proton.me>
2024-08-09 17:53:30 +0000

    fix: LocalPlayer():gmIntIsAdmin()

--- lua/gmod_integration/client/cl_main.lua
+++ lua/gmod_integration/client/cl_main.lua
@@ -17,7 +17,7 @@ function gmInte.showTestConnection(data)
 end
 
 function gmInte.openAdminConfig()
-    if !ply:gmIntIsAdmin() then
+    if !LocalPlayer():gmIntIsAdmin() then
         chat.AddText(Color(255, 130, 92), "[Gmod Integration] ", Color(102, 63, 63), "You are not superadmin")
         return
     end
