baadebd5176febd7463d02f145063f38d5460c9a
Linventif <gregoire.becue@proton.me>
2025-04-23 18:59:22 +0000

    refact: admin menu

--- lua/gmod_integration/core/config/cl_admin_menu.lua
+++ lua/gmod_integration/core/config/cl_admin_menu.lua
@@ -397,3 +397,11 @@ end
 
 concommand.Add("gmod_integration_admin", function() gmInte.SendNet("getConfig") end)
 concommand.Add("gmi_admin", function() gmInte.SendNet("getConfig") end)
+hook.Add("OnPlayerChat", "gmInte:OnPlayerChat:AdminCmd", function(ply, strText, bTeamOnly, bPlayerIsDead)
+    if ply != LocalPlayer() then return end
+    strText = string.lower(strText)
+    if strText == "/gmi" || strText == "!gmi" then
+        gmInte.openAdminConfig()
+        return true
+    end
+end)
--- lua/gmod_integration/core/config/cl_hook.lua
+++
@@ -1,8 +0,0 @@
-hook.Add("OnPlayerChat", "gmInte:OnPlayerChat:AdminCmd", function(ply, strText, bTeamOnly, bPlayerIsDead)
-	if ply != LocalPlayer() then return end
-	strText = string.lower(strText)
-	if strText == "/gmi" || strText == "!gmi" then
-		gmInte.openAdminConfig()
-		return true
-	end
-end)
