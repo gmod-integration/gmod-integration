c2c91362d11814c094476d46a5c492cb48cbf361
GitHub Action <action@github.com>
2025-07-03 18:45:58 +0000

    chore: bump version to 5.0.2

--- lua/autorun/gmod_integration.lua
+++ lua/autorun/gmod_integration.lua
@@ -28,7 +28,7 @@ else
 end
 
 gmInte = gmInte || {}
-gmInte.version = "5.0.1" // This will be automatically updated by GitHub Actions
+gmInte.version = "5.0.2" // This will be automatically updated by GitHub Actions
 gmInte.config = {}
 gmInte.useDataConfig = true
 function gmInte.simpleLog(msg, debug)
