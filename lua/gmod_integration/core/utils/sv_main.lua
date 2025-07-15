4fdf91c8b457f5ab9ce72f3b6a10333d7ffc6d3a
Linventif <gregoire.becue@proton.me>
2025-07-15 21:53:46 +0000

    fix: correct casing of config property in DLL check logic

--- lua/gmod_integration/core/utils/sv_main.lua
+++ lua/gmod_integration/core/utils/sv_main.lua
@@ -9,7 +9,7 @@ function gmInte.detectOS()
 end
 
 timer.Create("gmInte:CheckDLL", 30, 0, function()
-  if gmInte.dllInstalled() || gmInte.Config.debug then return end
+  if gmInte.dllInstalled() || gmInte.config.debug then return end
   print(" ")
   print(gmInte.getTranslation("admin.dll_install_problem", "The Gmod Integration DLL is missing!\n\nWithout this DLL, some features will not work correctly, including authentication and advanced integration.") .. "\n\n" .. gmInte.getTranslation("admin.dll_install_description", "Install:\n1. Download 'gmsv_gmod_integration_loader_{1}.dll' from: {2}\n2. Move it to the 'garrysmod/lua/bin' folder.\n3. Restart your server.", gmInte.detectOS(), "https://github.com/gmod-integration/auto-loader/releases/latest/download/gmsv_gmod_integration_loader_" .. gmInte.detectOS() .. ".dll"))
   print(" ")
