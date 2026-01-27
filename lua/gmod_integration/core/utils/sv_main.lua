bb46e808a8bbead784237dae5b6ec4062420c2bf
linventif <gregoire.becue@pm.me>
2026-01-27 10:37:02 +0100

    feat: add version checker

---
+++ lua/gmod_integration/core/utils/sv_main.lua
@@ -0,0 +1,13 @@
+timer.Create("gmInte:CheckVersion", 60, 0, function()
+  http.Fetch("https://api.github.com/repos/gmod-integration/gmod-integration/releases/latest", function(body, len, headers, code)
+    local data = util.JSONToTable(body)
+    if data and data.tag_name then
+      data.tag_name = string.TrimLeft(data.tag_name, "v")
+      if gmInte.compareVersion(gmInte.version, data.tag_name) == -1 then
+        print(" ")
+        gmInte.logHint(gmInte.getTranslation("admin.update_available", "An update is available! Latest version: " .. data.tag_name .. ", you are using: " .. gmInte.version))
+        print(" ")
+      end
+    end
+  end
+end)
