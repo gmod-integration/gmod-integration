ed957089407846242d591d2ca2a9b87adafcdbc1
linventif <gregoire.becue@proton.me>
2026-01-28 23:52:08 +0000

    remove: more dll references

--- lua/gmod_integration/core/net/cl_net.lua
+++ lua/gmod_integration/core/net/cl_net.lua
@@ -13,8 +13,6 @@ local netReceive = {
     ["publicConfig"] = function(data)
         gmInte.config = table.Merge(gmInte.config, data.config)
         gmInte.version = data.other.version
-        gmInte.dllExists = data.other.dllExists
-        if !gmInte.dllExists then gmInte.openDllInstall() end
         gmInte.loadTranslations()
         if gmInte.config.clientBranch != "any" && gmInte.config.clientBranch != BRANCH then gmInte.openWrongBranchPopup() end
         if !data.other.aprovedCredentials then RunConsoleCommand("gmod_integration_admin") end
